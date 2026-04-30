6d5a96abb8b3ffa792233b56244717e487f2fd64
NihghtHawk29 <86564227+NihghtHawk29@users.noreply.github.com>
2026-04-30 22:55:29 +0530

    fix vite config for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
+  }
 })
